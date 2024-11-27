56b940dce9cc3be9322088c86755b93b7f405396
David Hu-Liu <huliud9vid@gmail.com>
2024-11-27 04:49:51 -0500

    working diagonal ability, broken toggle turn

--- game.d
+++
@@ -1,4 +0,0 @@
-game.o: game.cc game.h board.h subject.h observer.h cell.h textobserver.h \
- player.h link.h ability/ability.h ability/linkboost.h ability/ability.h \
- ability/firewall.h ability/download.h ability/polarize.h ability/scan.h \
- ability/movediagonal.h ability/blackhole.h ability/extrastrength.h
--- textobserver.d
+++
@@ -1,2 +0,0 @@
-textobserver.o: textobserver.cc textobserver.h observer.h cell.h game.h \
- board.h subject.h
